95c668a4e388834d3abdf64fd7c3d993e4da8f02
Colin Walters <walters@verbum.org>
2021-05-03 20:56:37 -0400

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,35 @@
-use std::{fs::File, io::Write, path::Path};
-
-use anyhow::Result;
-use memmap2::{Mmap, MmapOptions};
+use anyhow::{anyhow, Context, Result};
+use camino::Utf8Path;
+use coreos_stream_metadata::Artifact;
+use rayon::prelude::*;
+use std::{collections::HashSet, convert::TryInto};
+use std::fs::File;
+use std::io::Write;
 use structopt::StructOpt;
 
-mod bupsplit;
+#[deny(unused_must_use)]
 mod rsync;
+
+// Number of CPUs we'll use
+const N_WORKERS: u32 = 2;
+const RSYNC_STRATEGY: &[&str] = &["openstack", "ibmcloud", "gcp"];
+
+// TODO: aws.vmdk is internally compressed.  We need to replicate the qemu-img arguments,
+// and also handle the CID.
+
+// TODO: vmware.ova is a tarball - of metadata followed by the compressed disk image.
+// To handle this fully reproducibly we'd need to try to regenerate the tarball
+// bit-for bit which may be ugly.  But since we know nothing is *after* the disk image,
+// it might work to literally save the tar headers and then generate the vmdk, then
+// concatenate the two.
 
 #[derive(Debug, StructOpt)]
 struct DehydrateOpts {
-    /// Path to qemu image
-    src_qemu: String,
-
-    dest_img: String,
-
-    output: String,
+//    #[structopt(long)]
+//    artifact: Vec<String>,
+    
+    #[structopt(long)]
+    skip_unavailable: bool,
 }
 
 #[derive(Debug, StructOpt)]
@@ -45,33 +60,106 @@ fn run() -> Result<()> {
 }
 
 fn rehydrate(opts: &RehydrateOpts) -> Result<(), anyhow::Error> {
-    let src = mmap(opts.src_qemu.as_str())?;
-    let patch = File::open(opts.patch.as_str())?;
-    let mut output = File::create(opts.output.as_str())?;
-    rsync::apply_patch(&src, patch, &mut output)?;
-    output.flush()?;
-    Ok(())
+    todo!()
 }
 
-fn mmap<P: AsRef<Path>>(p: P) -> Result<Mmap> {
-    let p = p.as_ref();
-    let f = File::open(p)?;
-    Ok(unsafe { MmapOptions::new().map(&f)? })
+fn uncompressed_name(s: &str) -> &str {
+    s.strip_suffix(".xz")
+        .or_else(|| s.strip_suffix(".gz"))
+        .unwrap_or(s)
+}
+
+fn filename_for_artifact(a: &Artifact) -> Result<&str> {
+    Ok(Utf8Path::new(&a.location)
+        .file_name()
+        .ok_or_else(|| anyhow!("Invalid artifact location: {}", a.location))?)
 }
 
 fn dehydrate(opts: &DehydrateOpts) -> Result<()> {
-    let src = mmap(opts.src_qemu.as_str())?;
-    let dest = mmap(opts.dest_img.as_str())?;
+    let s = File::open("stream.json").context("Failed to open stream.json")?;
+    let s: coreos_stream_metadata::Stream = serde_json::from_reader(std::io::BufReader::new(s))?;
+    let thisarch = s.this_architecture()
+        .ok_or_else(|| anyhow!("Missing this architecture in stream metadata"))?;
+    let qemu = s
+        .query_thisarch_single("qemu")
+        .ok_or_else(|| anyhow!("Missing qemu image"))?;
+    let qemu_fn = filename_for_artifact(qemu)?;
+    let qemu_fn = Utf8Path::new(uncompressed_name(qemu_fn));
+    if !qemu_fn.exists() {
+        return Err(anyhow!("Missing uncompressed qemu image: {}", qemu_fn));
+    }
+    let destdir = camino::Utf8Path::new("dehydrated");
+    std::fs::create_dir(destdir)
+        .with_context(|| anyhow!("Failed to create destination directory: {}", destdir))?;
+    std::fs::hard_link(qemu_fn, destdir.join(qemu_fn)).context("Hardlinking qemu image")?;
 
-    let mut output = File::create(opts.output.as_str())?;
-    let stats = rsync::create_patchfile(&src, &dest, &mut output)?;
-    output.flush()?;
+    let rsyncable: HashSet<_> = RSYNC_STRATEGY.iter().collect();
+    let rsyncable: Vec<&Artifact> = thisarch.artifacts.iter().filter_map(|(k, v)| if rsyncable.contains(&k.as_str()) {
+        Some(v.formats.get("disk").unwrap())
+    } else {
+        None
+    }).collect();
 
-    println!("{:?}", stats);
-    assert_eq!(
-        dest.len() as u64,
-        stats.matched_size + stats.unmatched_size
-    );
+    let platforms: Vec<&str> = 
+        s.this_architecture()
+            .unwrap()
+            .artifacts
+            .keys()
+            .map(|s| s.as_str())
+            .collect();
+
+
+    let metal = thisarch.artifacts.get("metal")
+        .ok_or_else(||anyhow!("Missing metal artifacts"))?;
+    let rsyncable: Vec<_> = artifacts.iter().filter(|a| rsyncable.contains(a)).collect();
+    rsyncable.push(metal.formats.get("raw.xz"));
+
+    println!("Processing artifacts: {}", artifacts.join(" "));
+
+    // Add some parallelism
+    let pool = rayon::ThreadPoolBuilder::new()
+        .num_threads(N_WORKERS as usize)
+        .build()
+        .unwrap();
+    pool.install(|| -> Result<_> {
+        let platforms = &["openstack", "gcp"];
+        platforms.par_iter().try_for_each(|artifact| {
+            let a = s
+                .query_thisarch_single(artifact)
+                .ok_or_else(|| anyhow!("Missing artifact {}", artifact))?;
+            let orig_fn = filename_for_artifact(a)?;
+            let orig_fn = Utf8Path::new(uncompressed_name(orig_fn));
+            if !orig_fn.exists() {
+                if opts.skip_unavailable {
+                    println!("Skipping: {}", orig_fn);
+                    return Ok(());
+                }
+                return Err(anyhow!("Missing image: {}", orig_fn));
+            }
+            let delta_path = &destdir.join(format!("{}.rdelta", orig_fn));
+            let mut output = std::io::BufWriter::new(File::create(delta_path)?);
+            rsync::prepare(qemu_fn, orig_fn, destdir, &mut output)?;
+            output.flush()?;
+            let orig_size = orig_fn.metadata()?.len() / (1000 * 1000);
+            let delta_size = delta_path.metadata()?.len() / (1000 * 1000);
+            println!(
+                "Dehydrated: {} ({}%, {} MB)",
+                orig_fn,
+                ((delta_size as f64 / orig_size as f64) * 100f64).trunc() as u32,
+                delta_size
+            );
+            Ok(())
+        })
+    })?;
+
+
+    let metal = if let Some(metal) = metal {
+        metal
+    } else {
+        if opts.skip_unavailable {
+            println!("Skipping metal artifacts")
+        }
+    }
 
     Ok(())
 }
@@ -81,5 +169,15 @@ fn main() {
     if let Err(e) = run() {
         eprintln!("{:#}", e);
         std::process::exit(1)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_strip_suffix() {
+        assert_eq!(uncompressed_name("foo.xz"), "foo");
     }
 }
--- src/rsync.rs
+++ src/rsync.rs
@@ -1,356 +1,54 @@
 //use rayon::prelude::*;
-use anyhow::{anyhow, Result};
-use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
+use anyhow::{anyhow, Context, Result};
+use camino::{Utf8Path, Utf8PathBuf};
 use serde_derive::{Deserialize, Serialize};
-use std::collections::{btree_map::Entry, BTreeMap, HashMap};
-use std::convert::{TryFrom, TryInto};
-use std::io::Read;
-use std::io::Write;
-
-use crate::bupsplit;
-
-const ROLLSUM_BLOB_MAX: usize = 8192 * 4;
-const HEADER: &str = "DELT";
-
-type CRC32 = u32;
-
-#[derive(Debug, PartialEq, Eq, Copy, Clone, Hash)]
-pub(crate) struct ChunkId {
-    pub(crate) crc32: CRC32,
-    pub(crate) len: u32,
-}
-
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
-pub(crate) struct Chunk<'a> {
-    pub(crate) start: u64,
-    pub(crate) buf: &'a [u8],
-}
-
-/// Create a mapping from CRC32 values to byte array chunks that match it.
-pub(crate) fn rollsum_chunks_crc32(mut buf_iter: &[u8]) -> HashMap<ChunkId, Vec<Chunk>> {
-    let mut ret = HashMap::<ChunkId, Vec<Chunk>>::new();
-    let mut done = false;
-    let mut start = 0u64;
-    while !buf_iter.is_empty() {
-        let ofs = if done {
-            buf_iter.len()
-        } else {
-            if let Some(ofs) = bupsplit::bupsplit_find_ofs(buf_iter) {
-                ofs
-            } else {
-                done = true;
-                buf_iter.len()
-            }
-        };
-        let ofs = ofs.min(ROLLSUM_BLOB_MAX);
-        let sub_buf = &buf_iter[..ofs];
-        let mut crc = crc32fast::Hasher::new();
-        crc.update(sub_buf);
-        let crc = crc.finalize();
-        let chunkid = ChunkId {
-            crc32: crc,
-            len: sub_buf.len().try_into().unwrap(),
-        };
-
-        let v = ret.entry(chunkid).or_default();
-        v.push(Chunk {
-            start,
-            buf: sub_buf,
-        });
-        start += ofs as u64;
-        buf_iter = &buf_iter[ofs..]
-    }
-    ret
-}
-
-/// Statistics.
-#[derive(Debug, Default, PartialEq, Eq)]
-pub(crate) struct RollsumDeltaStats {
-    pub(crate) matched_size: u64,
-    pub(crate) unmatched_size: u64,
-    pub(crate) crc_miss: u32,
-    pub(crate) chunkid_collision: u32,
-    pub(crate) src_chunks: u32,
-    pub(crate) dest_chunks: u32,
-    pub(crate) dest_duplicates: u32,
-    pub(crate) dest_size: u64,
-}
-
-#[derive(Debug, Serialize, Deserialize, Default)]
-pub(crate) struct Patch {
-    chunks: Vec<PatchEntry>,
-}
+use std::{convert::TryInto, io::Write};
+use std::{
+    fs::File,
+    process::{Command, Stdio},
+};
 
 #[derive(Debug, Serialize, Deserialize)]
-pub(crate) enum PatchEntry {
-    CopyBuf { start: u64, len: u64 },
-    CopySource { start: u64, len: u64 },
+struct RollsumDeltaHeader {
+    src_name: String,
+    dest_name: String,
 }
 
-/// rsync-style delta between two byte buffers.
-#[derive(Debug, Default)]
-pub(crate) struct RollsumDelta<'src, 'dest> {
-    pub(crate) matched: BTreeMap<u64, Chunk<'src>>,
-    pub(crate) unmatched_chunks: Vec<&'dest [u8]>,
-    // Maps from offset into unmatched_chunks
-    pub(crate) unmatched: BTreeMap<u64, usize>,
-    pub(crate) stats: RollsumDeltaStats,
-}
-
-fn dedup_chunks<'dest>(input: Vec<Chunk<'dest>>) -> (Vec<(u64, usize)>, Vec<&'dest [u8]>) {
-    let mut unique = Vec::<&[u8]>::new();
-    let mut ret = Vec::new();
-    for chunk in input {
-        let mut found = false;
-        for (i, &uniq) in unique.iter().enumerate() {
-            if chunk.buf == uniq {
-                ret.push((chunk.start, i));
-                found = true;
-                break;
-            }
-        }
-        if !found {
-            unique.push(chunk.buf);
-            ret.push((chunk.start, unique.len() - 1));
-        }
-    }
-    (ret, unique)
-}
-
-/// Compute an rsync-style delta between src and dest.
-pub(crate) fn rollsum_delta<'src, 'dest>(
-    src: &'src [u8],
-    dest: &'dest [u8],
-) -> RollsumDelta<'src, 'dest> {
-    let mut delta: RollsumDelta = Default::default();
-    let src_chunkset = rollsum_chunks_crc32(&src);
-    let mut dest_chunkset = rollsum_chunks_crc32(&dest);
-
-    delta.stats.src_chunks = src_chunkset.len().try_into().unwrap();
-    delta.stats.dest_chunks = dest_chunkset.len().try_into().unwrap();
-    delta.stats.dest_size = dest.len() as u64;
-
-    // We now have a mapping [CRC32] -> Vec<Chunk> for both source+destination.
-    // The goal is to find chunks in the source that we can reuse.  Retain
-    // in "dest_chunkset" all unmatched chunks
-    dest_chunkset.retain(|chunkid, dest_chunks| {
-        dest_chunks.retain(|dest_chunk| {
-            // Check to see if there are any chunks that match that CRC32+len in the source.
-            if let Some(src_chunks) = src_chunkset.get(&chunkid) {
-                // Loop over the source and destination chunks that match that CRC32+len
-                for src_chunk in src_chunks.iter() {
-                    // It's possible that the destination has duplicate
-                    // data; for example, uncompressed disk images may have
-                    // large runs of zero bytes.  Hence we now look to
-                    // see if we already found a match for this range in the
-                    // destination.  If so, we're done.  If not, do
-                    // a more expensive check to see if the buffer actually matches.
-                    match delta.matched.entry(dest_chunk.start) {
-                        Entry::Vacant(e) => {
-                            let len = src_chunk.buf.len();
-                            assert!(len > 0);
-                            // Directly compare the buffers.  If they're not equal, then
-                            // we obviously can't reuse it.  rsync uses md5 here, but
-                            // let's go for maximum reliablity and just bytewise compare.
-                            if src_chunk.buf != dest_chunk.buf {
-                                delta.stats.chunkid_collision += 1;
-                                continue;
-                            }
-                            e.insert(Chunk {
-                                buf: src_chunk.buf,
-                                start: src_chunk.start,
-                            });
-                            delta.stats.matched_size += len as u64;
-                            // We found a match, so don't keep this one in the unmatched set
-                            return false;
-                        }
-                        Entry::Occupied(_) => {
-                            panic!("Duplicate destination offset {}", dest_chunk.start);
-                        }
-                    }
-                }
-            } else {
-                delta.stats.crc_miss += 1;
-            }
-            true
-        });
-        // If we found matches for all chunks, remove this entry
-        !dest_chunks.is_empty()
-    });
-
-    for (_, dest_chunks) in dest_chunkset.into_iter() {
-        let origlen = dest_chunks.len();
-        delta.stats.unmatched_size += dest_chunks
-            .iter()
-            .fold(0u64, |acc, chunk| acc + chunk.buf.len() as u64);
-        let (dest_chunks, bufs) = dedup_chunks(dest_chunks);
-        delta.stats.dest_duplicates += origlen.checked_sub(bufs.len()).unwrap() as u32;
-        let offset = delta.unmatched_chunks.len();
-        delta.unmatched_chunks.extend(bufs);
-        delta
-            .unmatched
-            .extend(dest_chunks.into_iter().map(|(s, o)| (s, o + offset)));
-    }
-
-    delta
-}
-
-pub(crate) fn create_patchfile<W: std::io::Write>(
-    src: &[u8],
-    dest: &[u8],
-    out: W,
-) -> Result<RollsumDeltaStats> {
-    use itertools::{EitherOrBoth, Itertools};
-    let delta = rollsum_delta(src, dest);
-
-    let mut out = zstd::Encoder::new(out, 15)?;
-
-    let mut buflen = 0u64;
-    let mut chunk_offsets = HashMap::<usize, u64>::new();
-    for (i, chunk) in delta.unmatched_chunks.iter().enumerate() {
-        chunk_offsets.insert(i, buflen);
-        buflen += chunk.len() as u64;
-    }
-
-    out.write_all(HEADER.as_bytes())?;
-    out.write_u64::<LittleEndian>(buflen)?;
-
-    for &chunk in delta.unmatched_chunks.iter() {
-        out.write_all(chunk)?;
-    }
-
-    let mut patch = Patch {
-        ..Default::default()
-    };
-
-    for e in delta.matched.iter().zip_longest(delta.unmatched.iter()) {
-        let e = e.map_left(|(&start, chunk)| {
-            (
-                start,
-                PatchEntry::CopySource {
-                    start: chunk.start,
-                    len: chunk.buf.len() as u64,
-                },
-            )
-        });
-        let e = e.map_right(|(&start, &bufidx)| {
-            let len = delta.unmatched_chunks[bufidx].len() as u64;
-            let &buf_start = chunk_offsets.get(&bufidx).unwrap();
-            let p = PatchEntry::CopyBuf {
-                start: buf_start,
-                len,
-            };
-            (start, p)
-        });
-        match e {
-            EitherOrBoth::Both(matched, unmatched) => {
-                if matched.0 < unmatched.0 {
-                    patch.chunks.push(matched.1);
-                    patch.chunks.push(unmatched.1);
-                } else {
-                    patch.chunks.push(unmatched.1);
-                    patch.chunks.push(matched.1);
-                }
-            }
-            EitherOrBoth::Left(matched) => {
-                patch.chunks.push(matched.1);
-            }
-            EitherOrBoth::Right(unmatched) => patch.chunks.push(unmatched.1),
-        };
-    }
-
-    bincode::serialize_into(&mut out, &patch)?;
-
-    out.finish()?;
-
-    Ok(delta.stats)
-}
-
-pub(crate) fn apply_patch(
-    src: &[u8],
-    patchfile: impl std::io::Read,
-    mut dest: impl std::io::Write,
+pub(crate) fn prepare(
+    src: &camino::Utf8Path,
+    dest: &camino::Utf8Path,
+    tempdir: &camino::Utf8Path,
+    mut patch: impl Write,
 ) -> Result<()> {
-    let mut reader = zstd::Decoder::new(patchfile)?;
-    let mut buf = [0u8; 4];
-    reader.read_exact(&mut buf)?;
-    if buf != HEADER.as_bytes() {
-        return Err(anyhow!(
-            "Invalid header, expecting '{}' found '{:?}'",
-            HEADER,
-            buf
-        ));
+    let tempdir = tempfile::tempdir_in(tempdir).context("Creating tempdir")?;
+    let tempdir: &Utf8Path = tempdir.path().try_into()?;
+    let src_filename = src
+        .file_name()
+        .ok_or_else(|| anyhow!("Invalid source filename {}", src))?;
+    let dest_filename = dest
+        .file_name()
+        .ok_or_else(|| anyhow!("Invalid destination filename {}", dest))?;
+    let origdir = tempdir.join("orig");
+    std::fs::create_dir(&origdir)?;
+    std::fs::hard_link(src, origdir.join(src_filename)).context("Creating src hardlink")?;
+    let newdir = tempdir.join("new");
+    std::fs::create_dir(&newdir)?;
+    std::fs::hard_link(dest, newdir.join(src_filename)).context("Creating dest hardlink")?;
+
+    let out: Utf8PathBuf = tempdir.join("d");
+    let status = Command::new("rsync")
+        .arg("-a")
+        .arg(format!("--only-write-batch={}", out.as_str()))
+        .args(&[format!("{}/", origdir), format!("{}/", newdir)])
+        .stdin(Stdio::null())
+        .stdout(Stdio::null())
+        .status()?;
+    if !status.success() {
+        return Err(anyhow!("rsync failed: {:?}", status));
     }
-    let buflen = reader.read_u64::<LittleEndian>()?;
-    let (copybuf, reader) = {
-        let mut r = reader.take(buflen);
-        let mut v = Vec::with_capacity(buflen.try_into().unwrap());
-        let read = r.read_to_end(&mut v)? as u64;
-        if read < buflen {
-            return Err(anyhow!(
-                "Input truncated, expecting {} bytes for buffer, got {}",
-                buflen, read
-            ));
-        }
-        debug_assert_eq!(read, buflen);
-        (v, r.into_inner())
-    };
-    let patch: Patch = bincode::deserialize_from(reader)?;
-    for entry in patch.chunks {
-        match entry {
-            PatchEntry::CopyBuf { start, len } => {
-                let start: usize = start.try_into().unwrap();
-                let end = start + usize::try_from(len).unwrap();
-                let buf = &copybuf[start..end];
-                dest.write_all(buf)?;
-            }
-            PatchEntry::CopySource { start, len } => {
-                let start: usize = start.try_into().unwrap();
-                let end = start + usize::try_from(len).unwrap();
-                let buf = &src[start..end];
-                dest.write_all(buf)?;
-            }
-        }
-    }
+
+    let batchf = File::open(&out).with_context(|| format!("Reading delta file {}", out))?;
+    let mut batchf = std::io::BufReader::new(batchf);
+    std::io::copy(&mut batchf, &mut patch)?;
     Ok(())
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    const EMPTY: &[u8] = &[];
-    const SINGLE: &[u8] = &[42u8];
-
-    fn test_roundtrip(src: &[u8], dest: &[u8]) -> RollsumDeltaStats {
-        let mut delta = Vec::new();
-        let mut target = Vec::new();
-        let stats = super::create_patchfile(src, dest, &mut delta).unwrap();
-        super::apply_patch(src, delta.as_slice(), &mut target).unwrap();
-
-        stats
-    }
-
-    #[test]
-    fn test_rollsum() {
-        let delta = rollsum_delta(EMPTY, EMPTY);
-        assert_eq!(delta.matched.len(), 0);
-        let delta = rollsum_delta(SINGLE, SINGLE);
-        assert_eq!(delta.matched.len(), 1);
-        assert_eq!(
-            delta.matched.get(&0).unwrap(),
-            &Chunk {
-                buf: SINGLE,
-                start: 0,
-            }
-        );
-        let delta = rollsum_delta(EMPTY, SINGLE);
-        assert_eq!(delta.matched.len(), 0);
-    }
-
-    #[test]
-    fn test_delta_basic() {
-        test_roundtrip(EMPTY, EMPTY);
-        test_roundtrip(SINGLE, SINGLE);
-    }
 }
